02ecd50c2be415f180dea202e92eb39f0dc8159f
anne belka and steve burton <steve@>
2016-11-01 14:36:22 -0700

    add final javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,26 @@ Ticket.prototype.ticketDetails = function(){
 Ticket.prototype.findPrice = function(){
   if(this.age <= 13 && (this.movieName === "It Follows" || this.movieName === "Rocky Horror Picture Show")) {
     alert("You can't watch these adult films yet youngster!");
+    return this.price - 15;
   } else if(this.age <= 13) {
     return this.price - 11;
-    console.log(this.price - 11);
+  } else if(this.age >= 65){
+    return this.price - 7;
+  } else if(this.time === "2" || this.time === "12"){
+    return this.price - 4;
+  } else {
+    return this.price;
+  }
+}
+var showImage = function(movie){
+  if(movie === "Star Wars"){
+    return $("#darth").show();
+  } else if(movie === "Lo and Behold"){
+    return $("#behold").show();
+  } else if(movie === "It Follows"){
+    return $("#follows").show();
+  } else if(movie === "Rocky Horror Picture Show"){
+    return $("#rocky").show();
   }
 }
 
@@ -29,6 +46,7 @@ $(document).ready(function(){
     var price = newTicket.findPrice();
 console.log( newTicket);
     $("ul#result").append("<li><span class='movie'>" + newTicket.ticketDetails() + "<br> your price is $" + price + "</span></li>");
+    showImage(movie);
 
 
   });
